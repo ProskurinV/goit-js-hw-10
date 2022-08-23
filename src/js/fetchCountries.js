839e9d95bde8bf87f4b9e52ba8322cf83c30386b
ProskurinV <proskyrinv.g@gmail.com>
2022-08-23 18:25:23 +0300

    hw10Up2

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,20 +1,20 @@
-// export function fetchCountries(name) {
-//   const mainUrl = 'https:restcountries.com/v3.1/name/';
-//   const searchFilter = '?fields=name,capital,population,flags,languages';
-//   return fetch(`${mainUrl}${name}${searchFilter}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
 export function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
+  const mainUrl = 'https:restcountries.com/v3.1/name/';
+  const searchFilter = '?fields=name,capital,population,flags,languages';
+  return fetch(`${mainUrl}${name}${searchFilter}`).then(response => {
     if (!response.ok) {
-      throw Error(response.statusText);
+      throw new Error(response.status);
     }
     return response.json();
   });
 }
+// export function fetchCountries(name) {
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw Error(response.statusText);
+//     }
+//     return response.json();
+//   });
+// }
