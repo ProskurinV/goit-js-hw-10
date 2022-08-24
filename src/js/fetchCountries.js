264f2f00b69bfa7405552a37a65f61235dc6a47d
ProskurinV <proskyrinv.g@gmail.com>
2022-08-24 14:05:27 +0300

    /

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,7 @@
 export function fetchCountries(name) {
-  // const mainUrl = 'https:restcountries.com/v3.1/name/';
-  // const searchFilter = '?fields=name,capital,population,flags,languages';
-  return fetch(
-    `https:restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
+  const mainUrl = 'https://restcountries.com/v3.1/name/';
+  const searchFilter = '?fields=name,capital,population,flags,languages';
+  return fetch(`${mainUrl}${name}${searchFilter}`).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
